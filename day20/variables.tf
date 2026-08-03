1a18f3ea5def9300578a30162c68943a2f035b2f
jaydeepchavda <jaydipchavda2216@gmail.com>
2026-08-03 16:24:56 +0000

    feat: used vpc and its networking resource to create one vpc and working on it vpc project

---
+++ day20/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "company" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_cidr" {
+  type = string
+}
+
+variable "private_subnet_cidr" {
+  type = string
+}

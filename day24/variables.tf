ebdb63d9c652e01fe6c307d3cfd176c6b68a6e98
jaydeepchavda <jaydipchavda2216@gmail.com>
2026-08-07 17:32:18 +0000

    feat: terraform security group ec2 instance connection learning and apply in project

---
+++ day24/variables.tf
@@ -0,0 +1,42 @@
+variable "aws_region" {
+    type = string
+    default = "ap-south-1"
+}
+
+variable "aws_region" {
+    type = string
+    default = "ap-south-1"
+}
+
+
+variable "instance_type" {
+    type = string
+    default = "3.micro"
+}
+
+variable "vpc_id" {
+    type = string
+}
+
+
+variable "aws_region" {
+    type = string
+}
+
+
+variable "public_subnet_id" {
+    type = string
+}
+
+
+variable "my_ip" {
+    type = string
+}
+
+
+variable "key_name" {
+    type = string
+}
+
+
+

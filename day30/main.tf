ecea5bb29936a6ff4a70f53f4a3df715ba1ea5ec
jaydeepchavda <jaydipchavda2216@gmail.com>
2026-08-15 17:40:18 +0000

    feat: added lambda funcition with cloudwatch logs

---
+++ day30/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+
+resource "aws_iam_role" "lambda_execution" {
+  name = "lambda-demo-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "hello" {
+  function_name = "hello-lambda"
+
+  runtime = "python3.12"
+
+  handler = "lambda_function.lambda_handler"
+
+  role = aws_iam_role.lambda_execution.arn
+
+  filename = "lambda_function.zip"
+}
